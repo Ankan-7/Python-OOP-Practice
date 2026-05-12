127b3af607e3e3b9f34e705d752c22f20a2b9032
Ankan-7 <ankan3022@gmail.com>
2026-05-12 07:50:35 +0530

    Inheitance with Constructor Implemented

---
+++ inheritance_with_constructor.py
@@ -0,0 +1,11 @@
+class inherit1:
+    def __init__(self,name):
+        self.name=name
+    def k(self):
+        print("Hii",self.name)
+class inherit2(inherit1):
+    def l(self):
+        print("Hello",self.name)
+p=inherit2("Ankan")
+p.k()
+p.l()
